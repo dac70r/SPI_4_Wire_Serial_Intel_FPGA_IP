7ef9c46827dedf6e58555467d2672a8d632bd6a8
dac70r <denniswong178@gmail.com>
2025-04-30 10:39:39 +0800

    Generated NIOS II + Instantiation

--- spi_platform_designer/spi_platform_designer_bb.v
+++ spi_platform_designer/spi_platform_designer_bb.v
@@ -1,18 +1,22 @@
 
 module spi_platform_designer (
 	clk_clk,
-	reset_reset_n,
-	spi_pin_MISO,
-	spi_pin_MOSI,
-	spi_pin_SCLK,
-	spi_pin_SS_n,
-	spi_ss_manual_export);	
+	esc_eepdone_input_export,
+	esc_spi_MISO,
+	esc_spi_MOSI,
+	esc_spi_SCLK,
+	esc_spi_SS_n,
+	esc_spi_cs_export,
+	led_export,
+	reset_reset_n);	
 
 	input		clk_clk;
+	input		esc_eepdone_input_export;
+	input		esc_spi_MISO;
+	output		esc_spi_MOSI;
+	output		esc_spi_SCLK;
+	output		esc_spi_SS_n;
+	output		esc_spi_cs_export;
+	output		led_export;
 	input		reset_reset_n;
-	input		spi_pin_MISO;
-	output		spi_pin_MOSI;
-	output		spi_pin_SCLK;
-	output		spi_pin_SS_n;
-	output		spi_ss_manual_export;
 endmodule
